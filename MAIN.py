dfb5431cf72145e2c7c7b04104228ce8058bd1f5
Cristian19755 <82355909+Cristian19755@users.noreply.github.com>
2022-03-06 20:58:42 -0600

    Add files via upload

--- MAIN.py
+++ MAIN.py
@@ -1,4 +1,6 @@
-from tkinter import W, Button, Tk, filedialog
+from re import X
+from tkinter import W, Button, Pack, Tk, filedialog
+from typing import List
 from xml.dom import minidom
 import GrafPiso
 
@@ -13,7 +15,7 @@ class Lista:
 
     def insertar_inicio(self, data):
         self.root = node(data=data, siguiente=self.root) 
-
+ 
     def insertar_fin(self, midato): 
 
         if self.root is None: 
@@ -49,15 +51,39 @@ class Lista:
 
     def cantidad_de_datos(self):
         nodeAux = self.root 
-        contador=0
+        contador= 0
         while nodeAux != None:
             contador = contador+1
             nodeAux = nodeAux.siguiente
         return contador
+
+    
+class Listas2:
+    def remplazar_dato(y, x, dato):
+        auxtemp = y
+        temp = y.root
+        ini= y.cantidad_de_datos()
         
+        print(auxtemp)
+        for i in range(0,x):
+            temp = temp.siguiente
+            
+        temp.data = dato
+        y.root = temp
+        fin = y.cantidad_de_datos()
+    
+        print(auxtemp)
+        contador = 0
+        while ini != fin:
+            z = auxtemp.extraer_dato(contador)
+            y.insertar_inicio(z)
+            print(List)
+            contador = contador+1   
+            fin = List.cantidad_de_datos()        
+
 class main:
     def menu(self):
-        print("****Seleccione una opcion****\n1.Cargar Archivo\n2.Mostrar Patron\n3.Cambiar Patron\n4.Mostrar pisos cargados\n5.Salir")
+        print("****Seleccione una opcion****\n   1.Cargar Archivo\n   2.Salir")
         op = input()
         if op == '1':
             raiz = Tk()
@@ -67,20 +93,68 @@ class main:
                 self.archivo = minidom.parse(myArchivo)
                 M.leerArchivo()
                 print('Archivo Cargado')
-                M.menu()
+                M.menu2()
                 raiz.mainloop()
             else: 
                 print('Archivo no encontrado')
         if op == '2':
-            return
-        if op == '3':
-            x = int(input('ingrese el piso a graficar'))
-            y = int(input('ingrese el patron a graficar'))
-            M.graficar_piso(x, y)
-            M.menu
-        if op == '5':
             exit()
-    
+
+    def menu2(self):
+        print('****Seleccione un piso****\n  0. Salir')
+        for i in range(0,pisosLN.cantidad_de_datos()):
+            nombre = pisosLN.extraer_dato(i)
+            opcion = '  '+str(i+1)+'. '+nombre
+            print(opcion)
+        x=int(input())
+        if x == 0:
+            exit()
+        x= x-1
+        print('****Seleccione un patron inicial****\n  0. Salir')
+        for i in range(0,pisosLPC.extraer_dato(x).cantidad_de_datos()):
+            codigo = pisosLPC.extraer_dato(x).extraer_dato(i)
+            patron= pisosLP.extraer_dato(x).extraer_dato(i)
+            opcion = '  '+str(i+1)+'. '+codigo+': '+patron
+            print(opcion)
+        y = int (input()) 
+        if y == 0:
+            exit()
+        y = y-1
+        codigoin= pisosLPC.extraer_dato(x).extraer_dato(y)
+        patronin= pisosLP.extraer_dato(x).extraer_dato(y)
+        M.graficar_piso(x,y)
+        print('\n----------Grafica generada exitosamente----------\n')
+        print('****Seleccione un piso final****\n  0. Salir')
+        for i in range(0,pisosLPC.extraer_dato(x).cantidad_de_datos()):
+            codigo = pisosLPC.extraer_dato(x).extraer_dato(i)
+            patron= pisosLP.extraer_dato(x).extraer_dato(i)
+            opcion = '  '+str(i+1)+'. '+codigo+': '+patron
+            if i == y:
+                print(opcion+'  (patron Inicial)')
+            else:
+                print(opcion)
+        z = int (input()) 
+        if z == 0:
+            exit()
+        z = z-1
+        codigofin= pisosLPC.extraer_dato(x).extraer_dato(z)
+        patronfin = pisosLP.extraer_dato(x).extraer_dato(z)
+        f = FL.extraer_dato(x)
+        s = SL.extraer_dato(x)
+
+        
+        M.optimizacion(codigoin, codigofin, patronin,patronfin,f,s)
+        M.graficar_piso(x,z)
+        print('\n----------Grafica generada exitosamente----------\n')
+        print('**** Seleccione una opcion ****\n   1.Regresar al menu principal\n   2.Seleccionar otro piso\n   3.Salir')
+        opc = int(input())
+        if opc == 1 :
+            M.menu()
+        if opc == 2:
+            M.menu2()
+        if opc == 3:
+            exit()    
+
     def leerArchivo(self):
         a = self.archivo
         pisos = a.getElementsByTagName('piso')
@@ -117,12 +191,36 @@ class main:
                 j.insertar_fin(patronData)
             pisosLP.insertar_fin(j)
             pisosLPC.insertar_fin(k)
-        print(pisosLN, pisosLPC, pisosLP)
-
-    def optimizacion(self):
-        x = patronesL.extraer_dato(1)
-        print(x)
+    def voltear(self, x):
+        if x == 'W' or x == 'w':
+            return 'B'
+        if x == 'B' or x == 'b':
+            return 'W'
     
+    def optimizacion(self, COD1, COD2, PAT1, PAT2, f, s):
+        print('\n:::::::: PASOS '+COD1+' ---> '+COD2+' ::::::::\n')
+        contador = 0
+        for letra in PAT1:
+            PATRON1.insertar_fin(letra)
+        for letra in PAT2:
+            PATRON2.insertar_fin(letra)
+        contador = 0
+        PATRONAUX = Lista()
+
+        while contador < PATRON1.cantidad_de_datos():
+            if f == s:
+                if PATRON1.extraer_dato(contador) != PATRON2.extraer_dato(contador):
+                    x = PATRON1.extraer_dato(contador)
+                    PATRONAUX.insertar_fin(M.voltear(x))
+                    print('se volteo el piso #'+str(contador+1)+':')
+                    print(PATRONAUX)
+                else: 
+                    x = PATRON1.extraer_dato(contador)
+                    PATRONAUX.insertar_fin(x)
+                    print('se mantuvo el piso #'+str(contador+1)+':')
+                    print(PATRONAUX)
+            contador = contador +1
+
     def graficar_piso(self, x, y):
         cells = ''
         rows = ''
@@ -142,8 +240,9 @@ class main:
                 cells = ''
                 contador = 0
         GrafPiso.graficar.grafica(rows, nombre)
-        M.menu()
         
+PATRON1 = Lista()
+PATRON2 = Lista()      
 RL = Lista()
 CL = Lista()
 SL = Lista()
@@ -151,7 +250,5 @@ FL = Lista()
 pisosLPC= Lista()
 pisosLP = Lista()
 pisosLN = Lista()
-patronesLC = Lista()
-patronesL = Lista()
 M = main()
 M.menu()
